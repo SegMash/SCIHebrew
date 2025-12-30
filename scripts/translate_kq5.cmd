5ee8e0be61d865ab605b3bc18e28133e95dbae63
Segev.Mashraki@amdocs.com <Segev.Mashraki@amdocs.com>
2025-12-30 16:24:49 +0200

    fix script

--- scripts/translate_kq5.cmd
+++ scripts/translate_kq5.cmd
@@ -14,7 +14,7 @@ python scripts\extract_english.py temp_kq5 output_kq5
 REM 763 & 601  - I did manualy.
 for %%f in (0 29 63 89 113 119 123 216 220 600 602 610 659 663 664 673 749 754 755 756 889 950) do (
     python scripts\map_files.py .\output_kq5\%%f_english.txt .\output_kq5\%%f_hebrew.txt .\output_kq5\mapping_%%f.txt
-    python scripts\replace_in_tsv.py .\kq5_subs_project\kq5cd-text-patch\text\%%f.tsv .\output_kq5\mapping_%%f.txt .\output_kq5
+    python scripts\replace_in_tsv.py .\temp5_kq5\%%f.tsv .\output_kq5\mapping_%%f.txt .\output_kq5
     python scripts\tsv2tex.py .\output_kq5\%%f.tsv .\kq5_work --no-selector
 )
 REM Copy manuall tex files
