b057873dadd95ee56966a8de3bd501f56f8bbe43
Segev.Mashraki@amdocs.com <Segev.Mashraki@amdocs.com>
2026-01-14 15:07:11 +0200

    change text files

--- scripts/translate_kq4.cmd
+++ scripts/translate_kq4.cmd
@@ -2,9 +2,9 @@
 python scripts\map_files.py .\output_kq4\single_messages.txt .\output_kq4\single_messages_hebrew.txt .\output_kq4\single_mapping.txt
 python scripts\map_files.py --multiline .\output_kq4\multi_messages.txt .\output_kq4\multi_messages_hebrew.txt .\output_kq4\single_mapping.txt --append
 python scripts\map_files.py .\output_kq4\format_messages.txt .\output_kq4\format_messages_hebrew.txt .\output_kq4\single_mapping.txt --append
-python scripts\translate_texts.py kq4_resources kq4_work .\output_kq4\single_mapping.txt
+python scripts\translate_texts.py SCICompanion-3.2.4.0\kq4_resources patch_kq4\bin .\output_kq4\single_mapping.txt
 
-python.exe .\scripts\vocab_import.py output_kq4 kq4_work kq4_resources_copy
+python.exe .\scripts\vocab_import.py output_kq4 kq4_work patch_kq4\bin
 
 REM commands to create mapping file for built-in messages and to replace in gog sources
 REM python.exe .\scripts\map_files.py .\output_kq4\built-in-messages.txt .\output_kq4\built-in-messages_hebrew.txt .\output_kq4\built-in-mapping.txt
