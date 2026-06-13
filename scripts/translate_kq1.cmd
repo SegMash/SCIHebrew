851ffbcc3caefeb02252f4fe1e6832af65418a16
Segev.Mashraki@amdocs.com <Segev.Mashraki@amdocs.com>
2026-06-13 22:38:40 +0300

    fix script

--- scripts/translate_kq1.cmd
+++ scripts/translate_kq1.cmd
@@ -10,7 +10,7 @@ REM commands to create mapping file for built-in messages and to replace in gog 
 REM python.exe .\scripts\text\map_files.py .\output_kq1\built_in_messages_english.txt .\output_kq1\built_in_messages_hebrew.txt .\output_kq1\built-in-mapping.txt
 REM python.exe .\scripts\scripts\replace_strings.py .\kq1_new\src .\kq1_new\src_heb .\output_kq1\built-in-mapping.txt
 
-REM makensis.exe .\kq1_hebrew_patch.nsi
+makensis.exe .\kq1_hebrew_patch.nsi
 REM powershell -Command "(Get-FileHash -Algorithm MD5 'games_assets\kq1\bin\font.000').Hash.ToLower()"
 
 REM open issues:
